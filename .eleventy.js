32ccdb42f632fd2e26f72f15da93454cb6b46502
lemuelmgsn <magsinolemuel68@gmail.com>
2024-10-24 17:13:24 +0200

    setup the code for eleventy
    linked the css to the html/njk. made a layout template. added custom json scripts for easier terminal use.

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,18 @@
-export default function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css");
+module.exports = function(eleventyConfig) {
+    // zorgt ervoor dat de assets en css ook naar de public wordt gestuurd
+    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/assets");
       
-    
-    // Voeg watch targets toe om herbouw te triggeren bij wijzigingen
+    // Voeg watch targets toe om te herbouwen te bij wijzigingen
     eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/assets");
     
     // Return statement komt aan het einde
     return {
         dir: {
+            // alle files waar je in werkt
             input: "src",
+            // alle files die uitkomen
             output: "public",
         },
     };
